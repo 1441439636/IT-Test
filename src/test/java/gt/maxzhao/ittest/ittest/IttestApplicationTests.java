f263bb21bebce4e49b5728ca565969309b090a74
maxzhao <1441439636@qq.com>
2019-02-14 18:55:10 +0800

    basic

---
+++ src/test/java/gt/maxzhao/ittest/ittest/IttestApplicationTests.java
@@ -0,0 +1,17 @@
+package gt.maxzhao.ittest.ittest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IttestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
